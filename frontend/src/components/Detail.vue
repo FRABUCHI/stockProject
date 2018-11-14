c776d3fc86236b72f7e9731ffecc7440cf58fe9f
혁준 <gurwnsdlgud@ajou.ac.kr>
2018-11-15 01:07:08 +0900

    stock/detail 업데이트

---
+++ frontend/src/components/Detail.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="detail">
+     <h1>여긴 Detail 페이지임니당~ㅎ</h1>
+     <br><br>
+     <b-table striped hover
+         :items="detail_items"
+         :fields="detial_fields">
+    </b-table>
+    <br><br>
+    <img src="@/assets/exgraph.png" 
+         width="1300dp" height="500dp" style="margin-left:250px"/>
+
+    </div>
+</template>
+
+
+
+<script> 
+let detail_items = [
+  {주식명: "혁준이", 시가: 7, 고가: 777, 저가: 444, 종가: 789, 거래량: 777777},
+];
+
+
+
+export default {
+  name: "App",
+  data() {
+    return {
+      detail_fields: ["주식명", "시가", "고가", "저가", "종가", "거래량"],
+      detail_items: detail_items,
+    }
+  },
+  methods: {
+    getStockDetail() {},
+    showAlert () { }
+  }
+};
+
+
+</script>
--- frontend/src/components/Stock.vue
+++ frontend/src/components/Stock.vue
@@ -1,10 +1,17 @@
 <template>
     <div class='Stock'>
+      <h1>여긴 Stock 페이지입니당~~</h1>
         <b-nav-form>
            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-           <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+           <b-button size="sm" class="my-2 my-sm-0" type="submit">검색</b-button>
         </b-nav-form>
-
+        <b-alert :show="dismissCountDown"
+             dismissible
+             variant="warning"
+             @dismissed="dismissCountDown=0"
+             @dismiss-count-down="countDownChanged">
+          <p>즐겨찾기에 추가되었습니다.</p>
+         </b-alert>
         <b-tabs>
             <b-tab title="관심종목" active>
                 <b-table striped hover
@@ -13,7 +20,8 @@
                          :current-page="currentPage"
                          :per-page="perPage">
                           <template slot="상세보기" slot-scope="row">
-                           <b-button size="sm" v-on:click="'/detail/:stockId'" class="mr-2">
+                           <b-button size="sm" v-on:click="'/detail/:stockId'" 
+                                     variant="info" class="mr-2">
                                 보기
                            </b-button>
                           </template>
@@ -26,12 +34,13 @@
                          :current-page="currentPage"
                          :per-page="perPage">
                           <template slot="상세보기" slot-scope="row">
-                           <b-button size="sm" v-on:click="'detail/:stockId'" class="mr-2">
+                           <b-button size="sm" v-on:click="'detail/:stockId'" 
+                                     class="mr-2" variant="info">
                                 보기
                            </b-button>
                           </template>
-                          <template slot="즐겨찾기" slot-scope="row">
-                           <b-button size="sm" v-on:click="'/detail/:stockId'" class="mr-2">
+                          <template slot="즐겨찾기" slot-scope="row"> 
+                           <b-button size="sm" class="mr-2" @click="showAlert" variant="info">
                                 추가
                            </b-button>
                           </template>
@@ -49,18 +58,18 @@
 
 <script>
 let like_items = [
-  { 주식명: "삼성전자", 시가: 100, 종가: 300, 고가: 150 },
-  { 주식명: "LG전자", 시가: 120, 종가: 340, 고가: 120 },
-  { 주식명: "SK하이닉스", 시가: 130, 종가: 270, 고가: 200 },
-  { 주식명: "아주대", 시가: 105, 종가: 500, 고가: 720 },
-  { 주식명: "Tonez", 시가: 111, 종가: 222, 고가: 333 }
+  {주식명: "삼성전자", 시가: 100, 종가: 300, 고가: 150 },
+  {주식명: "LG전자", 시가: 120, 종가: 340, 고가: 120 },
+  {주식명: "SK하이닉스", 시가: 130, 종가: 270, 고가: 200 },
+  {주식명: "아주대", 시가: 105, 종가: 500, 고가: 720 },
+  {주식명: "Tonez", 시가: 111, 종가: 222, 고가: 333 }
 ];
 let total_items = [
-  { 주식명: "A", 시가: 100, 종가: 300, 고가: 150 },
-  { 주식명: "B", 시가: 120, 종가: 340, 고가: 120 },
-  { 주식명: "C", 시가: 130, 종가: 270, 고가: 200 },
-  { 주식명: "D", 시가: 105, 종가: 500, 고가: 720 },
-  { 주식명: "E", 시가: 111, 종가: 222, 고가: 333 }
+  {주식명: "A", 시가: 100, 종가: 300, 고가: 150 },
+  {주식명: "B", 시가: 120, 종가: 340, 고가: 120 },
+  {주식명: "C", 시가: 130, 종가: 270, 고가: 200 },
+  {주식명: "D", 시가: 105, 종가: 500, 고가: 720 },
+  {주식명: "E", 시가: 111, 종가: 222, 고가: 333 }
 ];
 
 export default {
@@ -71,12 +80,22 @@ export default {
       total_fields: ["주식명", "시가", "종가", "고가", "상세보기", "즐겨찾기"],
       like_items: like_items,
       total_items: total_items,
+      likeRows: like_items.length,
+      totalRows: total_items.length,
       currentPage: 1,
-      perPage: 3
-    };
+      perPage: 3,
+      dismissSecs: 1,
+      dismissCountDown: 0
+    }
   },
-  method: {
-    getStockDetail() {}
+  methods: {
+    getStockDetail() {},
+    countDownChanged (dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+    showAlert () {
+      this.dismissCountDown = this.dismissSecs
+    }
   }
 };
 </script>
