b736ca67409049016d6aaf505ca9ed6708521d13
혁준 <gurwnsdlgud@ajou.ac.kr>
2018-11-25 08:35:31 +0900

    로고 삽입, 디테일/스톡 페이지 레이아웃
    ㅇ

--- frontend/src/components/Detail.vue
+++ frontend/src/components/Detail.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="detail">
-      <h1>   상세보기: {{"  "+company}}</h1>
-     <br><br>
+      <h1> 상세보기: {{"  "+company}}</h1>
+      <hr align="center" style="border: solid 0.5px black; width: 90%;">
+     <br>
      <div class="table"> 
      <b-table striped hover
               :fields="detail_fields"
@@ -9,27 +10,31 @@
      </b-table>
      </div>
     <br><br>
-    <div style="width:500px; height:250px; border:1px solid red; float:left; margin-left:80px">
-      <img src="@/assets/exgraph.png" align="center" style="margin-left: auto; margin-right: auto; display: block;"/>
+    <div class="bottomset">
+     <div style="width:600; height:300px; float:left; margin-right:80px">
+       <img src="@/assets/exgraph.png" align="center" style="margin-left: auto; margin-right: auto; display: block;"/>
+     </div>
+
+     <div style="width:500px;height:250px; float:right; margin-left:80px">
+       <b-list-group>
+         <br><br><br><br><br>
+        <b-list-group-item class="d-flex justify-content-between align-items-center">
+          <strong> 예측 가격 : {{" "+stockUp.predict_price+ " 원"}}</strong> 
+        </b-list-group-item><br>
+        <!--
+        <b-list-group-item class="d-flex justify-content-between align-items-center">
+          <strong> 한달 후 : {{" "+cost.month + " 원"}}</strong>
+        </b-list-group-item><br>
+        <b-list-group-item class="d-flex justify-content-between align-items-center">
+          <strong> 6개월 후 : {{" "+cost.half_year + " 원"}}</strong>
+        </b-list-group-item><br>
+        <b-list-group-item class="d-flex justify-content-between align-items-center">
+          <strong> 1년 후 : {{" "+cost.year + " 원"}}</strong>
+        </b-list-group-item>!-->
+      </b-list-group>
+     </div>
     </div>
-    <div style="width:500px;height:250px; float:right; margin-right:80px">
-      <b-list-group>
-        <br>
-       <b-list-group-item class="d-flex justify-content-between align-items-center">
-         <strong> 1일 후 : {{" "+stockUp.predict_price+ " 원"}}</strong> 
-       </b-list-group-item><br>
-       <b-list-group-item class="d-flex justify-content-between align-items-center">
-         <strong> 한달 후 : {{" "+cost.month + " 원"}}</strong>
-       </b-list-group-item><br>
-       <b-list-group-item class="d-flex justify-content-between align-items-center">
-         <strong> 6개월 후 : {{" "+cost.half_year + " 원"}}</strong>
-       </b-list-group-item><br>
-       <b-list-group-item class="d-flex justify-content-between align-items-center">
-         <strong> 1년 후 : {{" "+cost.year + " 원"}}</strong>
-       </b-list-group-item>
-     </b-list-group>
-    </div>
-    </div>
+  </div>
 </template>
 
  
@@ -110,5 +115,9 @@ export default {
 .table{
   margin-left:auto;
   margin-right:auto;
+}
+.bottomset{
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
