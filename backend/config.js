84fef6c6cf3ccf85be6a9a8a2067e06fadff822b
문수 <lms1427@gmail.com>
2018-12-02 17:20:59 +0900

    Money vue update

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'SeCrEtKeYfOrHaShInG',
-    'mongodbUri': 'mongodb://localhost:27017/MrStock'
-    //'mongodbUri': 'mongodb://52.79.240.43:27017/MrStock'
+    //'mongodbUri': 'mongodb://localhost:27017/MrStock'
+    'mongodbUri': 'mongodb://52.79.240.43:27017/MrStock'
 }
